00681c7e6599cf15516bd0708b33859fa5591892
je-lam <jemian.lam@gmail.com>
2024-10-21 11:32:02 -0700

    Week5 javascript writing

---
+++ week5/index.js
@@ -0,0 +1,20 @@
+// console.log("Hello world!");
+// alert("Hello, world!");
+window.document.write("<p>Welcome to CSEN 60!</p>");
+
+//* Append only wrks if you're referencing the specific element on the page
+// window.document.append("<p>This should come after the h1</p>");
+
+/*
+this
+is
+a
+multi-line
+comment
+*/
+
+// TODO: fix this function
+
+function add(a, b) {
+    return a + b;
+}
