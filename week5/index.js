2ef9544da4d0d0b187693bf9a6bb61ded70de79c
je-lam <jemian.lam@gmail.com>
2024-10-23 11:34:18 -0700

    More JS practice work during class

--- week5/index.js
+++ week5/index.js
@@ -18,3 +18,55 @@ comment
 function add(a, b) {
     return a + b;
 }
+
+// This is the variable declaration
+let x = 100;
+// let x => Declares x as a spot in the memory
+// x = 2 => This is the initialization. 
+if (true) {
+    x = 200; //same variable
+}
+console.log(x);
+
+let y;
+y = null
+let z = 0
+console.log(y, z)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date().getHours()}`;
+console.log(response);
+
+const age = 20
+
+if (age > 18) {
+    console.log("You are of age.")
+} else {
+    console.log(`You aren't of age. Please wait ${18 - age} years`);
+}
+
+// Another example of this would be showing the differences between a logged in/out version of a webpage.
+// (suggested by professor Xanthus)
+
+
+
+/* outdated stuff */
+// let isAvailable = true;
+// let isOverAge = (age > 18);
+
+// console.log("\n", isOverAge);
+
+// THIS is an example of an Object. https://www.w3docs.com/learn-javascript/javascript-data-types.html
+// According to Xanthus, these are basically structs.
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person.age)
+console.log(person["age"]) //does same as the above, useful for long names and such.
+console.log(typeof ("hello world"));
+
+console.log(3 === '3') //false, fails one test
+console.log(3 === 2) //also false, fails another test
