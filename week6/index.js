cb001b053c44b6ca1c82895f971696cbc746a811
je-lam <jemian.lam@gmail.com>
2024-10-28 11:35:16 -0700

    Added todo function

---
+++ week6/index.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("list-input")
+console.log(input.value);
+
+function addTodo() {
+    // console.log("button was clicked!")
+    const input = document.getElementById("list-input")
+    console.log("Input value = ", input.value)
+    // const list = document.getElementsByTagName("ul")
+    // console.log(list)
+
+    const ul = document.querySelector("ul")
+    console.log(ul)
+
+    const li = document.createElement("li")
+    li.textContent = input.value
+    ul.appendChild(li)
+}
